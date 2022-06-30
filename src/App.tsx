2d4b2d5492f41dbf44ebc0c17995e249e4ed564b
donqlee <donqlee88@gmail.com>
2022-06-30 14:54:18 +0900

    feat: 디자인 시스템 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, CampDetail, CampApply, Community, NotFound } from "pages";
+import GlobalStyled from "styles/global";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/chillingcode">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/camp/apply" element={<CampApply />} />
-        <Route path="/camp/:id" element={<CampDetail />} />
-        <Route path="/community" element={<Community />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyled />
+      <BrowserRouter basename="/chillingcode">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/camp/apply" element={<CampApply />} />
+          <Route path="/camp/:id" element={<CampDetail />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
---
+++ src/styles/global.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "./reset";
+
+export default createGlobalStyle`
+  ${reset}
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+    background-color: white;
+    letter-spacing: -0.5px;
+    touch-action: pan-y;
+    -webkit-font-smoothing: antialiased;
+  }
+`;
