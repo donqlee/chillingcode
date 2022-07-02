29d1e5c9db9d881731f6afd3d9f2e54be89a0e8f
donqlee <donqlee88@gmail.com>
2022-07-02 23:05:00 +0900

    feat: Home 페이지 HeaderSection 구현

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,25 @@
-import { Navigation } from "components";
-import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+import { Navigation, Footer } from "components";
+import { HeaderSection } from "./components/HeaderSection";
+import { ICamp } from "types/type";
+import { useEffect, useState } from "react";
+
+const campMock: ICamp = {
+  id: 0,
+  name: "업무 단순화 & 자동화로 엑셀을 실무에 더 적극 활용하기",
+  type: "인기",
+  status: "open",
+  field: "데이터분석",
+  skill: "Excel",
+  startDate: "2021-03-13",
+  thumbnail: "https://cdn.comento.kr/images/pt/tmp/prefix_44UsYDVNuM.jpg",
+};
 const Home = () => {
   return (
     <div>
       <Navigation />
-      <Link to="/camp/1">
-        <div>Camp 1</div>
-      </Link>
-      <Link to="/camp/2">
-        <div>Camp 2</div>
-      </Link>
-      <Link to="/camp/3">
-        <div>Camp 3</div>
-      </Link>
+      <HeaderSection />
     </div>
   );
 };
