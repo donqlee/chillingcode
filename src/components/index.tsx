e097e96fb20b8b2d03e8febfe13b40076a7351d9
donqlee <donqlee88@gmail.com>
2022-07-05 23:32:17 +0900

    feat: Tags 컴포넌트 추가

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as Navigation } from "./Navigation";
 export { default as CampCard } from "./CampCard";
 export { default as Footer } from "./Footer";
 export { default as Padding } from "./Padding";
+export { default as Tags } from "./Tags";
