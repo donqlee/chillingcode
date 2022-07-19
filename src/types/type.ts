7c22c2cd3de6dbb81b92cb29c22bcfc3a9309000
donqlee <donqlee88@gmail.com>
2022-07-19 15:53:07 +0900

    feat: ICampDetail Type 추가

--- src/types/type.ts
+++ src/types/type.ts
@@ -9,6 +9,25 @@ export interface ICamp {
   startDate: string;
   thumbnail: string;
 }
+export interface ICampDetail {
+  id: number;
+  name: string;
+  tags: string[];
+  desc: string;
+  seat: string;
+  reviewMaterial: string[];
+  headerImaget: string;
+  images: string[];
+  process: string;
+  type: CampType;
+  status: string;
+  field: string;
+  skill: string;
+  startDate: string;
+  thumbnail: string;
+  reviews: { content: string; reviewer: string }[];
+  faqs: { question: string; answer: string }[];
+}
 export interface ICommunity {
   id: number;
   tags: string[];
